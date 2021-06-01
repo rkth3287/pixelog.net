f7c4bce665eef43fbc4a2f0b4b2b5c41eca50dae
onissyuta <onissyuta@gmail.com>
2021-06-01 20:29:16 +0900

    :wrench: AmazonアソシエイトID変更

--- themes/hexo-theme-kusatsu/scripts/amazon.js
+++ themes/hexo-theme-kusatsu/scripts/amazon.js
@@ -1,5 +1,5 @@
 hexo.extend.tag.register('amazon', function(args){
-    const id = 'pixelognet-22'
+    const id = 'pixelog04-22'
     const asin = args[0];
     const title = args[1];
     return `<p><a href="https://www.amazon.co.jp/dp/${asin}/?tag=${id}"><img src="https://m.media-amazon.com/images/P/${asin}.jpg" alt="${title}" width="400"><br>${title}</a></p>`  
